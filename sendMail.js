dde03962c60e838930e88ffd2a8632a9358dff06
deep <shahdeep966@gmail.com>
2024-01-18 11:05:31 +0530

    Update code for creds

--- sendMail.js
+++ sendMail.js
@@ -7,8 +7,8 @@ const sendMail = (zone, currentMarket, mmi, lastWeekMmi, lastMonthMmi) => {
 
   return new Promise((resolve, reject) => {
     gmailSend({
-      user: credentials.user,
-      pass: credentials.password,
+      user: credentials?.user || process.env.user,
+      pass: credentials?.password || process.env.password,
       to: credentials.user,
       subject: 'MMI ALERT - ' + currentMarket,
       text: title,
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+const cron = require('node-cron');
 const handler = require("./handler")
+const http = require("http")
 
-handler.mmiAlert()
+// Schedule the cron job
+cron.schedule('0 20 * * 1-5', () => {
+  // This will run your function at 8 PM (20:00) from Monday to Friday (1-5) // 0 20 * * 1-5
+  handler.mmiAlert();
+}, {
+  scheduled: true,
+  timezone: 'Asia/Kolkata', // Replace with your actual timezone, e.g., 'America/New_York'
+});
+
+http.createServer((req, res) => {
+  res.write("Market Alerts is Live -> Sending mail...")
+  handler.mmiAlert();
+  res.end();
+}).listen(process.env.PORT);
+
+console.log("Market Alert script is live now at ", process.env.PORT)
